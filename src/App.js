5503c2b5d9d00bf7422f9433efac6af06b725e24
stevenjchang <stevenjchang@gmail.com>
2019-07-28 12:53:27 -0700

    move components to components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StateManagement from './StateManagement';
+import StateManagement from './components/StateManagement';
 
 function App() {
   return (
